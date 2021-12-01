37513f49671d68ea23ef493795a0a33de8c33c05
Norman Liu <norman@chronicled.com>
2021-11-30 23:40:23 -0800

    gen boilerplate

---
+++ src/AOC2021/Solutions/Day16.hs
@@ -0,0 +1,11 @@
+module AOC2021.Solutions.Day16 (part1, part2) where
+
+import AOC2021.Prelude
+import Data.Map.Lazy qualified as Map
+import Data.Text qualified as Text
+
+part1 :: String -> String
+part1 = const "UNIMPLEMENTED"
+
+part2 :: String -> String
+part2 = const "UNIMPLEMENTED"
