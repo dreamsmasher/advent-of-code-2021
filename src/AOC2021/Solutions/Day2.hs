190080c2b5a3162ca8f9ea825dbe4a16aa400966
Norman Liu <norman@chronicled.com>
2021-12-01 21:50:38 -0800

    day 2 solved

--- src/AOC2021/Solutions/Day2.hs
+++ src/AOC2021/Solutions/Day2.hs
@@ -4,8 +4,28 @@ import AOC2021.Prelude
 import Data.Map.Lazy qualified as Map
 import Data.Text qualified as Text
 
+data Dir = Forward | Down | Up deriving (Eq, Show, Read, Enum)
+
+data Command = Command Dir Int deriving (Eq, Show)
+data Position = Pos {
+  depth :: Int,
+  horiz :: Int,
+  aim :: Int
+} deriving (Eq, Show, Ord)
+
+walk :: (Position -> Int -> (Position, Position, Position)) -> String -> String
+walk step = show . mul . foldl' go (Pos 0 0 0) . parseLines parse
+  where parse (words -> [dir, n]) = Command (fromJust $ readLower dir) (read n)
+        mul (Pos d h _) = d * h
+        go pos (Command d n) = 
+          let (fwd, down, up) = step pos n 
+            in case d of
+                Forward -> fwd
+                Down -> down
+                _ -> up
+
 part1 :: String -> String
-part1 = const "UNIMPLEMENTED"
+part1 = walk \(Pos d h a) n -> (Pos d (h + n) a, Pos (d + n) h a, Pos (d - n) h a)
 
 part2 :: String -> String
-part2 = const "UNIMPLEMENTED"
+part2 = walk \(Pos d h a) n -> (Pos (d + n * a) (h + n) a, Pos d h (a + n), Pos d h (a - n))
