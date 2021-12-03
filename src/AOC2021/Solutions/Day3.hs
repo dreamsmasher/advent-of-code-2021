4eac6b3d7e867e99670b4e0fc7c62cad1c940517
Norman Liu <norman@chronicled.com>
2021-12-02 23:17:21 -0800

    day 3 solved

--- src/AOC2021/Solutions/Day3.hs
+++ src/AOC2021/Solutions/Day3.hs
@@ -1,11 +1,35 @@
-module AOC2021.Solutions.Day3 (part1, part2) where
+module AOC2021.Solutions.Day3  where
 
 import AOC2021.Prelude
 import Data.Map.Lazy qualified as Map
 import Data.Text qualified as Text
+import Data.Bits 
 
-part1 :: String -> String
-part1 = const "UNIMPLEMENTED"
+part1 :: String -> Int
+part1 = ((*) `uncurryOn` fromBits)
+      . unzip 
+      . map (dupWith not . ((<) `uncurryOn` length) . partition id) 
+      . transpose 
+      . parseLines (map (== '1'))
 
-part2 :: String -> String
-part2 = const "UNIMPLEMENTED"
+fromBits :: Enum a => [a] -> Int
+fromBits = foldl' (\a x -> a `shiftL` 1 .|. fromEnum x) 0
+
+unconsAll :: [[a]] -> ([a], [[a]])
+unconsAll xs = (map head xs, map tail' xs)
+
+findRating :: (Ordering -> Bool) -> [([Bool], [Bool])] -> Int
+findRating cmp = fromBits 
+               . fst 
+               . Map.findMin 
+               . head 
+               . dropWhile ((/= 1) . length) 
+               . iterate go' 
+               . Map.fromList
+  where go' st = 
+          let (ones, zeros) = Map.partition head $ Map.filter (not . null) st
+              target = cmp $ (compare `on` length) zeros ones
+            in fmap tail' $ if target then ones else zeros
+
+part2 :: String -> Int
+part2 = (liftA2 (*) `on` (findRating . ($ GT))) (==) (/=) . map dup . parseLines (map (== '1'))
