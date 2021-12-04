fb0a4be10b0912dcd31c87e2a16a6056a761e956
Norman Liu <norman@chronicled.com>
2021-12-03 23:22:36 -0800

    finally implement main

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE NoOverloadedLists #-}
 module Main where
 
 import System.Environment
 import AOC2021.Prelude
 import AOC2021.Config (getOpts)
+import Text.Printf
 import Data.Text qualified as Text
 import Advent 
 import AOC2021.Runner (resolveSolutionsFor)
+import Data.Foldable (traverse_)
 
 orExit :: MonadFail m => m (Maybe a) -> String -> m a
 orExit ma msg = ma >>= maybe (fail msg) pure
@@ -17,6 +20,17 @@ toPart :: Integral n => n -> Part
 toPart 1 = Part1
 toPart _ = Part2
 
+runAoC' :: AoCOpts -> AoC a -> ExceptT ClientError IO a
+runAoC' = (ExceptT . fmap (first AoCErr)) .: runAoC
+
+data ClientError = AoCErr AoCError | ClientErr String deriving (Show)
+
+confirmOrBreak :: (MonadError ClientError m, MonadIO m) => String -> m ()
+confirmOrBreak msg = do
+  let msg' = msg <> " [Y,n]"
+  input <- liftIO (putStrLn msg' *> getLine)
+  when (input == "n") . throwError $ ClientErr "User abort"
+
 main :: IO ()
 main = do
   args <- getArgs
@@ -25,15 +39,38 @@ main = do
     _ -> fail "Could not parse arguments. USAGE: advent2021 [day] [part]"
   day <- pure day `orExit` "Day needs to be within 1 <= d <= 25"
   aocOpts <- getOpts `orExit` "Could not initialize configuration for advent of code session, exiting!" 
-  input <- fmap Text.unpack . unwrapE . runAoC aocOpts $ AoCInput day
 
-  let (part1, part2) = resolveSolutionsFor . fromIntegral $ dayInt day
-      part1Sol = part1 input
-      part2Sol = part2 input
-  displayPretty 1 part1Sol
-  displayPretty 2 part2Sol
-  
-  pure ()
+  let (solve1, solve2) = resolveSolutionsFor . fromIntegral $ dayInt day
+      solve = case part of
+        Part1 -> solve1
+        Part2 -> solve2
+      log :: MonadIO m => String -> m ()
+      log = liftIO . putStrLn
+
+  aocRes <- runExceptT do
+    input <- fmap Text.unpack . runAoC' aocOpts $ AoCInput day
+    log $ printf "Running day %d part %d..." (dayInt day) (partInt part)
+    let !soln = solve input
+    log $ "\nResult: " <> soln <> "\n"
+    confirmOrBreak "Submit?"
+    (_, result) <- runAoC' aocOpts $ AoCSubmit day part (solve input)
+    log $ fmtResult result
+
+  case aocRes of
+    Left (AoCErr error) -> 
+      traverse_ log ["Received server error:", show error]
+    _ -> pure ()
+
+fmtResult :: SubmitRes -> String
+fmtResult = 
+  let fmt = printf @(String -> String) "-- %s --" . capitalize
+      [right, wrong, error] = map fmt ["correct", "incorrect", "error"]
+   in unlines . \case
+        SubCorrect mbRank -> [right, maybe "" (printf "Congrats! You ranked #%d globally!") mbRank]
+        SubIncorrect delay errMsg -> [wrong, errMsg ?? "", printf "You can retry in %d seconds" delay]
+        SubWait delay -> [wrong, printf "Tried to resubmit too soon! Try again in %d seconds." delay]
+        SubInvalid -> [wrong, "Invalid submission. Maybe you submitted to a part you've already solved or haven't unlocked?"]
+        SubUnknown err -> [wrong, "Error parsing server response, received err:"] <> map ("  " <>) (lines err)
 
 displayPretty :: Int -> String -> IO ()
 displayPretty part res = putStrLn $ "--- PART " <> show part <> " ---\n" <> res
