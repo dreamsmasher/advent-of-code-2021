7131f2a5a09b33301f0cc4f79e1acde651a416b7
Norman Liu <norman@chronicled.com>
2021-12-05 21:47:53 -0800

    solve day 5, day 6

--- src/AOC2021/Solutions/Day5.hs
+++ src/AOC2021/Solutions/Day5.hs
@@ -1,11 +1,35 @@
-module AOC2021.Solutions.Day5 (part1, part2) where
+module AOC2021.Solutions.Day5 where
+-- module AOC2021.Solutions.Day5 (part1, part2, parseLineRepr, testData) where
 
 import AOC2021.Prelude
 import Data.Map.Lazy qualified as Map
 import Data.Text qualified as Text
+import Data.Ord (comparing)
+import Data.Foldable (traverse_)
 
-part1 :: String -> String
-part1 = const "UNIMPLEMENTED"
+data Line = Line {start :: (Int, Int), end :: (Int, Int)} deriving (Eq, Show)
 
-part2 :: String -> String
-part2 = const "UNIMPLEMENTED"
+parseLineRepr :: String -> Line
+parseLineRepr = uncurryP Line order2 . both (get2 . map read . splitOn ",") . get2 . splitOn " -> "
+  where get2 (x : y : _) = (x, y)
+
+axisAligned :: Line -> Bool
+axisAligned (Line (x1, y1) (x2, y2)) = x1 == x2 || y1 == y2
+
+solveDay5 :: Foldable t => ([Line] -> t Line) -> String -> Int
+solveDay5 f = length . Map.filter (> 1) . countElems . concatMap getPoints . f . parseLines parseLineRepr
+
+mkRange :: (Ord a, Enum a) => a -> a -> [a]
+mkRange x y = case compare x y of
+  LT -> [x..y]
+  EQ -> [x]
+  GT -> reverse [y..x]
+
+getPoints :: Line -> [(Int, Int)]
+getPoints (Line (x1, y1) (x2, y2)) = alignWith (these (, y1) (x1, ) (,)) (mkRange x1 x2) (mkRange y1 y2)
+
+part1 :: String -> Int
+part1 = solveDay5 $ filter axisAligned
+
+part2 :: String -> Int
+part2 = solveDay5 id
--- src/AOC2021/Solutions/Day6.hs
+++ src/AOC2021/Solutions/Day6.hs
@@ -1,11 +1,19 @@
-module AOC2021.Solutions.Day6 (part1, part2) where
+module AOC2021.Solutions.Day6 where
 
 import AOC2021.Prelude
 import Data.Map.Lazy qualified as Map
 import Data.Text qualified as Text
 
-part1 :: String -> String
-part1 = const "UNIMPLEMENTED"
+stepFish :: Map.Map Int Int -> Map.Map Int Int
+stepFish fishes = 
+  let (zeros, rest) = Map.alterF (\z -> (z ?? 0, Nothing)) 0 fishes
+  in Map.insert 8 zeros . Map.insertWith (+) 6 zeros $ Map.mapKeys pred rest
 
-part2 :: String -> String
-part2 = const "UNIMPLEMENTED"
+solveDay6 :: Int -> String -> Int
+solveDay6 n = sum . Map.elems . iterateTimes n stepFish . countElems . map read . splitOn ","
+
+part1 :: String -> Int
+part1 = solveDay6 80
+
+part2 :: String -> Int
+part2 = solveDay6 256
