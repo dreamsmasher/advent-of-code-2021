d595dadb012eec2eb4f6ca4741fe7da610ff1330
Norman Liu <norman@chronicled.com>
2021-12-06 21:27:05 -0800

    solve day 7

--- src/AOC2021/Solutions/Day7.hs
+++ src/AOC2021/Solutions/Day7.hs
@@ -4,8 +4,18 @@ import AOC2021.Prelude
 import Data.Map.Lazy qualified as Map
 import Data.Text qualified as Text
 
-part1 :: String -> String
-part1 = const "UNIMPLEMENTED"
+solveDay7 :: (Int -> Int -> Int) -> String -> Int
+solveDay7 f = minimum . getDists . parseCommas
+  where getDists xs =
+          let counts = countElems xs
+              lim = maximum xs
+              getDist n = sum $ Map.mapWithKey (\k v -> abs (f k n) * v) counts
+           in map getDist [0..lim] 
 
-part2 :: String -> String
-part2 = const "UNIMPLEMENTED"
+part1 :: String -> Int
+part1 = solveDay7 subtract
+
+part2 :: String -> Int
+part2 = solveDay7 countDistance
+  where sumTo n = (n * (n + 1)) `shiftR` 1 -- thank you based Leibniz
+        countDistance x y = sumTo $ abs (x - y)
